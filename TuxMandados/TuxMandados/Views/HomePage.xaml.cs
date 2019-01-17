2c4c53d60db6e287d755dffd98e58754487a8112
EDREY WEBSTER CR <EACR_777@hotmail.com>
2019-01-17 16:13:07 -0600

    Order
    Vista del detalle del mandado

--- TuxMandados/TuxMandados/Views/HomePage.xaml.cs
+++ TuxMandados/TuxMandados/Views/HomePage.xaml.cs
@@ -1,27 +1,14 @@
 ﻿namespace TuxMandados.Views
 {
     using Xamarin.Forms;
-    using Syncfusion.XForms.ProgressBar;
+   
     public partial class HomePage : ContentPage
     {
         public HomePage()
         {
             InitializeComponent();
-            this.RangeColorWithGradientProgressBar.AnimationDuration = 2000;
+            
         }
-        private void RangeColorWithGradiantProgressBar_ValueChanged(object sender, ProgressValueEventArgs e)
-        {
-            if (e.Progress.Equals(100))
-            {
-                this.RangeColorWithGradientProgressBar.SetProgress(0, 0, Easing.Linear);
-                
-            }
-
-            if (e.Progress.Equals(0))
-            {
-                this.RangeColorWithGradientProgressBar.Progress = 100;
-                
-            }
-        }
+       
     }
 }

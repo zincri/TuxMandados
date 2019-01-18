4d3f8c8bd3eefaa926db1a99a88a787c728a0fa1
EDREY WEBSTER CR <EACR_777@hotmail.com>
2019-01-18 14:46:18 -0600

    eacr
    About

---
+++ TuxMandados/TuxMandados/ViewModels/AboutViewModel.cs
@@ -0,0 +1,85 @@
+﻿
+
+namespace TuxMandados.ViewModels
+{
+    using System;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using TuxMandados.Views;
+    using Xamarin.Forms;
+    public class AboutViewModel : INotifyPropertyChanged
+    {
+
+        #region Vars  
+        private bool _isEnable;
+        private string _correo;
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Properties
+        public string Correo
+        {
+            get
+            {
+                return _correo;
+            }
+            set
+            {
+                _correo = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsEnable
+        {
+            get
+            {
+                return _isEnable;
+            }
+            set
+            {
+                _isEnable = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+        #region Constructors
+        public AboutViewModel()
+        {
+            this.Correo = "correo@hotmail.com";
+        }
+        #endregion
+
+        #region Commands
+
+        public ICommand SendCommand
+        {
+            get
+            {
+                return new RelayCommand(SendMethod);
+            }
+        }
+
+
+
+        #endregion
+
+
+
+        #region Methods       
+        private async void SendMethod()
+        {
+            await App.Current.MainPage.DisplayAlert("Éxito", "Correo enviado", "ok");
+            return;
+        }
+        private void OnPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}

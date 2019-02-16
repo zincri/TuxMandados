2a844570ce2bfaea3cc46109be095d38a888f0bc
Zincri <zincri_mlz@hotmail.com>
2019-02-15 20:24:27 -0600

    Zincri
    Zincri

---
+++ TuxMandados.API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace TuxMandados.API.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
